326f488270fdf1134ad6402c4d8c5c7eed7fa6ab
Yuri Gavshin <juri.gavsin@ttu.ee>
2013-07-26 16:29:20 +0300

    Updated a bit...

--- scripts/intellibrainbot.py
+++ scripts/intellibrainbot.py
@@ -9,11 +9,12 @@ import threading
 class intellibrainbot:
     def callback_serial_in(self, data):
         rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
-        
-    def addCommand(self, cmd)
+
+    def addCommand(self, cmd):
         self.threadLock.acquire()
         self.commands.append(cmd)
         self.threadLock.release()
+        #print ('Added command "'+ cmd + '"')
    
     def __init__(self, rate = 10, port='/dev/ttyUSB0', baudrate=115200, 
         parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS):
@@ -21,8 +22,9 @@ class intellibrainbot:
         self.ser.open()
         self.ser.isOpen()
         self.rate = rate
-        self.commands=list()
         self.threadLock = threading.Lock()
+        self.commands=list()
+        self.addCommand('D 0 0\n')
         self.addCommand('R L R C O\n')
         rospy.init_node('intellibrainbot')
         rospy.Subscriber("serial_in", String, self.callback_serial_in)
@@ -47,20 +49,27 @@ class intellibrainbot:
                 self.threadLock.acquire()
                 cmd = self.commands.pop(0)
                 self.threadLock.release()
+            
             if cmd!=None:
+                #print ('Sending command "'+ cmd + '"')
                 self.ser.write(cmd)
             
             data=''
             t = ''        
             while t!='\n' and not rospy.is_shutdown():
+                #print ('Waiting '+ str(self.ser.inWaiting()) +' "' + data + '"' )
                 if self.ser.inWaiting() > 0:
                     t = self.ser.read(1)
                     if (t!='\r' and t!='\n'):
-                        data+=t	
-
-            str_ = data + ' ' + str(self.ser.inWaiting())
-            rospy.loginfo(str_)
-            self.pub_serial_out.publish(String(str_))
+                        data+=t
+                elif len(data)==0:
+                    break
+                
+            if len(data)>0:
+                str_ = data + ' ' + str(self.ser.inWaiting())
+                rospy.loginfo(str_)
+                self.pub_serial_out.publish(String(str_))
+                
             r.sleep()
 
 if __name__ == '__main__':

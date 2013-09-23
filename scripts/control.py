9ed80225a71d1ecdb7625b2515764e7b43f797fb
Yuri Gavshin <yury@yury-wind.(none)>
2013-09-23 14:33:46 +0300

    Alpha version of control tutorial in C++ and Python

--- scripts/control.py
+++ scripts/control.py
@@ -2,12 +2,21 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 import tf   #for a function to transform quaternion into euler
 import math #for sqrt and atan2 functions
 
+
+def normalizeAngle(a){
+    while a < -math.pi:
+        a += 2.0*math.pi
+    while a >  math.pi:
+        a -= 2.0*math.pi	 
+    return a
+
 lastOdomReading = None
 
-def callback(data):
+def odometryReceived(data):
     global lastOdomReading 
     lastOdomReading = data
     
@@ -15,61 +24,65 @@ def control():
     global lastOdomReading
     rospy.init_node('control')
 
-    rospy.Subscriber("odom", Odometry, callback)
+    rospy.Subscriber("odom", Odometry, odometryReceived)
     pub = rospy.Publisher('cmd_vel', Twist)
-
+    pub_p = rospy.Publisher('p', Float64)
     r = rospy.Rate(10) # 10hz
+    
+    #getting coefficients from Parameter Server
+    Kp = rospy.get_param('Kp',0.3)
+    Ka = rospy.get_param('Ka',0.8)
+    Kb = rospy.get_param('Kb',-0.15)
+
+    #goal pose: coordinates (x,y) in metres and orintation (th) in radians
+    goal_x = rospy.get_param('~x',0)
+    goal_y = rospy.get_param('~y',0)
+    #goal_th = rospy.get_param('~th',0) #unused for now and equals to 0
+
+    cmd = Twist() #command that will be sent to Stage (published)
+    
     while not rospy.is_shutdown():
-	if lastOdomReading == None:#we cannot issue any commands until we have our position
+        if lastOdomReading == None:#we cannot issue any commands until we have our position
             print 'waiting for lastOdomReading to become available'
             r.sleep()
             continue 
-
-	#goal pose: coordinates (x,y) in metres and orintation (th) in radians
-        goal_x = rospy.get_param('~x',0)
-        goal_y = rospy.get_param('~y',0)
-        goal_th = rospy.get_param('~th',0) #unused for now and equals to 0
-
-	#current robot 2D coordinates and orientation
+        
+        #current robot 2D coordinates and orientation
         pose = lastOdomReading.pose.pose #robots current pose (position and orientation)
         x = pose.position.x
         y = pose.position.y
         #euler_from_quaternion returns array of [pitch, roll, yaw], we need only yaw (rotation around Z axis)
-	th = tf.transformations.euler_from_quaternion([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])[2]
+        th = tf.transformations.euler_from_quaternion([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])[2]
 
         #deltas
         dx = goal_x - x
-	dy = goal_y - y
-	dth = goal_th - th
+        dy = goal_y - y
+        dth = goal_th - th
 
+        print x, y, th
         if math.fabs(dx)<0.01 and math.fabs(dy)<0.01 and math.fabs(dth)<0.01:
             print 'Reached destination'
             break
-        else:
-            print x, y, th
-	
+        
         #control equations (slides 23 and 24)
         p = math.sqrt(dx*dx + dy*dy)
-        a = math.fmod(-th + math.atan2(dy,dx),2*math.pi) 
-        if a>math.pi:
-            a-=2*math.pi
-        b = math.fmod(-th - a,2*math.pi) 
-        if b>math.pi:
-            b-=2*math.pi
+        a = normalizeAngle(-th + math.atan2(dy,dx)) 
+        b = normalizeAngle(-th - a) 
+        v = Kp*p #translational speed in m/s
+        w = normalizeAngle(Ka*a + Kb*b) #rotational speed in rad/s
+        #setting command fields
+        cmd.linear.x = v 
+        cmd.angular.z = w 
+        #publishing command to a robot
+        pub.publish(cmd);
+    
+        #publishing p for rqt_plot
+        Float64 p_
+        p_.data = p
+        pub_p.publish(p_)
         
-        #getting coefficients from Parameter Server
-        Kp = rospy.get_param('Kp',0.3)
-        Ka = rospy.get_param('Ka',0.8)
-        Kb = rospy.get_param('Kb',-0.15)
-
-        #calculating speeds
-        cmd = Twist() #command that will be sent to Stage (published)
-        cmd.linear.x = Kp*p #v, translational speed in m/s
-	cmd.angular.z = Ka*a + Kb*b #w - rotational speed in rad/s
-
-        pub.publish(cmd)
         r.sleep()
         
+
 if __name__ == '__main__':
     control()
-
--- src/control.cpp
+++ src/control.cpp
@@ -1,13 +1,104 @@
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float64.h>
+
+#include <cmath>
+#include <tf/tf.h> //to transform quaternion into euler
+
+
+double normalizeAngle( double a ){
+    while( a < -M_PI ) a += 2.0*M_PI;
+    while( a >  M_PI ) a -= 2.0*M_PI;	 
+    return a;
+};
+
+nav_msgs::OdometryConstPtr lastOdomReading;
+
+void odometryReceived(nav_msgs::OdometryConstPtr msg){
+  lastOdomReading = msg;
+}
 
 int 
 main(int argc, char** argv)
 { 
-  ros::init(argc, argv, "stageros");
-  ros::Rate rate(10);
+  
+  ros::init(argc, argv, "control");
+  ros::NodeHandle node;
+  ros::NodeHandle pnode("~");
+  ros::Subscriber sub = node.subscribe<nav_msgs::Odometry>("odom",10, odometryReceived);  
+  ros::Publisher pub = node.advertise<geometry_msgs::Twist>("cmd_vel", 1, false);
+  ros::Publisher pub_p = node.advertise<std_msgs::Float64>("p", 1, false);
+  ros::Rate r(10);
+  
+  geometry_msgs::Twist cmd; //command that will be sent to Stage (published)
+  
+  //getting coefficients from Parameter Server
+  double Kp = 0.3;
+  double Ka = 0.8;
+  double Kb = -0.15;
+  node.getParam("Kp",Kp);
+  node.getParam("Ka",Ka);
+  node.getParam("Kb",Kb);
+  
+  //goal pose: coordinates (x,y) in metres and orintation (th) in radians
+  double goal_x = 0;
+  double goal_y = 0;
+  double goal_th = 0;
+  pnode.getParam("x",goal_x);
+  pnode.getParam("y",goal_y);
+  //pnode.getParam("th",goal_th);//th is 0 for now, since control equation has hardcoded final orientation
+  
   while(ros::ok())
   {
-    rate.sleep();
+    //required in C++ clients to receive messages from subscribed topics
+    //note: Python clients don't need this call
+    ros::spinOnce();
+    
+    if (!lastOdomReading){
+      //waiting for an Odometry reading
+      std::cout<<"waiting for lastOdomReading to become available"<<std::endl;
+      r.sleep();
+      continue;
+    }
+    
+    //current robot 2D coordinates and orientation
+    geometry_msgs::Pose pose = lastOdomReading->pose.pose;//robots current pose (position and orientation)
+    double x = pose.position.x;
+    double y = pose.position.y;
+    double th = tf::getYaw(pose.orientation);
+    
+    //deltas
+    double dx = goal_x - x;
+    double dy = goal_y - y;
+    double dth = normalizeAngle(goal_th - th);
+    
+    std::cout<<x<<" "<<y<<" "<<th<<std::endl;
+    if (fabs(dx)<0.01 && fabs(dy)<0.01 && fabs(dth)<0.01){
+      std::cout<<"Reached destination"<<std::endl;
+      break;
+    }
+
+    //control equations (slides 23? and 24?)
+    double p = sqrt(dx*dx + dy*dy);
+    double a = normalizeAngle(-th + atan2(dy,dx)); 
+    double b = normalizeAngle(-th - a); 
+    double v = Kp*p; //translational speed in m/s
+    double w = normalizeAngle(Ka*a + Kb*b); //rotational speed in rad/s
+    
+    //setting command fields
+    cmd.linear.x = v;
+    cmd.angular.z = w;
+    
+    //publishing command to a robot
+    pub.publish(cmd);
+    
+    //publishing p for rqt_plot
+    std_msgs::Float64 p_;
+    p_.data = p;
+    pub_p.publish(p_);
+    
+    r.sleep();
   }
   return 0;
 }
